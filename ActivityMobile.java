f448af7819bb5254d2f7a0449c09e0c630385820
harrishx2 <harrishx2@gmail.com>
2025-12-05 10:25:37 +0530

    Add files via upload
    Mobile program to take photo

---
+++ ActivityMobile.java
@@ -0,0 +1,13 @@
+package test1;
+
+public class ActivityMobile {
+
+	public void takephoto() {
+		System.out.println("take photo");
+    }
+
+	public static void main(String[] args) {
+		ActivityMobile obj=new ActivityMobile();
+		obj.takephoto();
+	}
+}
